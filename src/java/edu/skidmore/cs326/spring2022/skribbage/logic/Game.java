bf8ba4d8900c1e4a1a03b5702e04b05da0bf7c76
Shriner <mshriner@cslab05.cs.skidmore.edu>
2022-03-02 08:56:17 -0500

    you can compile now

--- src/java/edu/skidmore/cs326/spring2022/skribbage/logic/Game.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/logic/Game.java
@@ -1,36 +1,47 @@
-/**
-@author Michael Shriner 
-*/
-public class Game{
-
-  Deck theDeck;
-
-  ArrayList <Player> playerList = new ArrayList <Player> ();
-
-  public Game(int numPlayers){
-    //initialize theDeck
-    this.theDeck = new Deck();
-
-    //initialize list of players, given umPlayers
-    initPlayers(numPlayers);
-
-  }
-
-  public void initPlayers(int numPlayers){
-
-    for (int i = 0; i < numPlayers; i++){
-      Player p = new Player();
-      playerList.add(p);
-    }
-
-  }//end of initPlayers
-
-  //write method for count hand phase
-
-  //write method for deal phase
-
-  //write method for pegging play
-
-
-
-}
+// /**
+// Game class has a deck and a list of players. It initializes the deck and list of players.
+// In the future, it may function as a starting point for each phase of the game.
+// @author Michael Shriner
+// */
+// public class Game{
+//
+//   private Deck theDeck;
+//
+//   private ArrayList <Player> playerList = new ArrayList <Player> ();
+//
+//   /**
+//   Constructor creates a Game, initializes the deck, and initializes the list of players
+//   for this game.
+//   @param numPlayers is the number of players
+//   */
+//   public Game(int numPlayers){
+//     //initialize theDeck
+//     this.theDeck = new Deck();
+//
+//     //initialize list of players, given umPlayers
+//     initPlayers(numPlayers);
+//
+//   }
+//
+//   /**
+//   Initializes the ArrayList of Player objects given the number of players for this game.
+//   @param numPlayers is the number of players
+//   */
+//   private void initPlayers(int numPlayers){
+//
+//     for (int i = 0; i < numPlayers; i++){
+//       Player p = new Player();
+//       playerList.add(p);
+//     }
+//
+//   }//end of initPlayers
+//
+//   //write method for count hand phase
+//
+//   //write method for deal phase
+//
+//   //write method for pegging play
+//
+//
+//
+// }
