dac9717e56a1bc5640be19bd387e3273c2d4eeda
ACarney <paridisedwarfhigh@hotmail.com>
2022-03-08 23:03:47 -0500

    Added prototyped architecture

--- src/java/edu/skidmore/cs326/spring2022/skribbage/common/EventType.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/common/EventType.java
@@ -1,5 +1,12 @@
 package edu.skidmore.cs326.spring2022.skribbage.common;
 
+/**
+ * An enum representing all possible events. Whenever a new event type is
+ * written, its associated title must be included with this enum.
+ *
+ * @author Alex Carney
+ *
+ */
 public enum EventType {
     USER_LOGIN,
     USER_LOGIN_HASHED,
