437ff7e78e4850db0e77156d0c72670ca83836d4
Almheiri <malmhei1@cslab20.cs.skidmore.edu>
2022-03-11 09:18:21 -0500

    checkstyle and logging

--- src/java/edu/skidmore/cs326/spring2022/skribbage/gamification/ItemShopInterface.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/gamification/ItemShopInterface.java
@@ -1,49 +1,49 @@
-//package edu.skidmore.cs326.spring2022.skribbage.gamification;
-//
-//import org.apache.log4j.Logger;
-//
-//import java.io.*;
-//import java.util.HashMap;
-//
-///**
-// *  Item Shop Interface
-// *  TODO Add update methods to update player tokens after purchase and update player inventory.
-// * @author Muaded Almheiri
-// *
-// */
-//public interface ItemShopInterface {
-//	
-//    /**
-//     * Hash map with store items being sold with their token price.
-//     */
-//	public HashMap<String, Integer> storeItems = new HashMap<String, Integer>();
-//	
-//	/**
-//	 * Print Item name, price, and description.
-//	 */
-//	public void getItemInfo();
-//	
-//	/**
-//	 * Buy's item if player has enough tokens.
-//	 * @param playerTokens Tokens held by player.
-//	 */
-//	public void buyItem(int playerTokens);
-//	
-//	/**
-//	 * Getter method for special card/item name.
-//	 * @return special card/item name
-//	 */
-//	public String getName();
-//	
-//	/**
-//	 * Getter method for special card/item token price.
-//	 * @return item token price
-//	 */
-//	public int getPrice();
-//	
-//	/**
-//	 * Getter method for special card/item description.
-//	 * @return special card/item description/use.
-//	 */
-//	public String getDescription();
-//}
+package edu.skidmore.cs326.spring2022.skribbage.gamification;
+
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.util.HashMap;
+
+/**
+*  Item Shop Interface
+*  TODO Add update methods to update player tokens after purchase and update player inventory.
+* @author Muaded Almheiri
+*
+*/
+public interface ItemShopInterface {
+
+   /**
+    * Hash map with store items being sold with their token price.
+    */
+	public HashMap<String, Integer> storeItems = new HashMap<String, Integer>();
+
+	/**
+	 * Print Item name, price, and description.
+	 */
+	public void getItemInfo();
+
+	/**
+	 * Buy's item if player has enough tokens.
+	 * @param playerTokens Tokens held by player.
+	 */
+	public void buyItem(int playerTokens);
+
+	/**
+	 * Getter method for special card/item name.
+	 * @return special card/item name
+	 */
+	public String getName();
+
+	/**
+	 * Getter method for special card/item token price.
+	 * @return item token price
+	 */
+	public int getPrice();
+
+	/**
+	 * Getter method for special card/item description.
+	 * @return special card/item description/use.
+	 */
+	public String getDescription();
+}
