6d5ee0160783a59e2e60664e3539410f86601d09
Lois Appiah-Agyeman <lappiaha@Loiss-Air.skidmore.edu>
2022-03-21 17:22:33 -0400

    updated player

--- src/java/edu/skidmore/cs326/spring2022/skribbage/logic/Player.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/logic/Player.java
@@ -1,4 +1,6 @@
+
 import java.util.ArrayList;
+
 import java.util.Random;
 
 /**
@@ -6,179 +8,207 @@ import java.util.Random;
  */
 public class Player {
 	boolean isDealer;
-	String player1;
-	String player2;
+	String pone;
+	String dealer;
+	int points = 0; 
+
 	private ArrayList<Card> cards = new ArrayList<Card>();
-	ArrayList<Card> dealfive = new ArrayList<Card>();
+	private ArrayList<Card> withdrawn = new ArrayList<Card>();
+	private Hand hand = new Hand();
+	private Deck thedeck = new Deck();
+	
 
 
 	public Player() {
-		//		//Assuming that he cut happened already
-		//		if (PlayerCard.getpointValue() > OpponentCard.getpointValue()) {
-		//			isDealer = true;
-		//		}
-		//		else {
-		//			isDealer = false;
-		//		}
+	//initializes the player 
+		
+	//initializes the deck
+		this.thedeck = new Deck();
+	
 	}
-
-	public void ClaimName() {
+/**
+ * Get the name of the player
+ * 
+ */
+	public void getName() {
+		//need info from frontEnd 
 		//		player1 = name1.getName();
 		//		player2 = name2.getName();
 		//claims the player name 
 	}
+/**
+ * Set the name of the player
+ * 
+ */
+	public void setName() {
+		
+	}
+	public void setHand(Hand playerHand) {
 
-	public void setHand() {
-		Random rand = new Random();
-
-		for(int i = 0; i < cards.size(); i++) {
-			i = rand.nextInt(0, cards.size());
-			dealfive.add(cards.get(i));
-			dealfive.add(cards.get(i));
-			dealfive.add(cards.get(i));
-			dealfive.add(cards.get(i));
-			dealfive.add(cards.get(i));
-			cards.remove(i);
-		}
+//		thedeck.removeTopCard();
+		hand = playerHand ; 
 
 	}
 
-
-public ArrayList<Card> getHand() {
-	return dealfive;
-}
-
-
-
-public void CountPoints() {
-	//points act as counter
-	int points; 
-	boolean is3OAK;
-	boolean ispair;
-	//get deck
-	//count player points
-
-	//pairs
-	ArrayList<Card> firsttwo = new ArrayList<Card>(dealfive.subList(0, 1));
-	for(int i=0; i < firsttwo.size(); i++) {
-		if(firsttwo.getIdentifier().equals(firsttwo.get(i).getIdentifier())) {
-			ispair = true;
-		}
-	}
-
-	ArrayList<Card> secondtwo = new ArrayList<Card>(dealfive.subList(1, 2));
-	for(int i=0; i < secondtwo.size(); i++) {
-		if(secondtwo.getIdentifier(i).equals(getIdentifier(i+1))) {
-			ispair = true;
-		}
-	}
-
-	ArrayList<Card> middletwo = new ArrayList<Card>(dealfive.subList(2, 3));
-	for(int i=0 ; i < middletwo.size(); i++) {
-		if(middletwo.getIdentifier(i).equals(middletwo.getIdentifier(i+1))) {
-			ispair = true;
-		}
-	}
-	ArrayList<Card> lasttwo = new ArrayList<Card>(dealfive.subList(2, 3));
-	for(int i=0 ; i < lasttwo.size(); i++) {
-		if(lasttwo.getIdentifier(i).equals(lasttwo.getIdentifier(i+1))) {
-			ispair = true;
-		}
-	}
-
-	if(ispair = true) {
-		points += 2;
+/**
+ * Get the hand
+ * @return hand
+ *  
+ */
+	public Hand getHand() {
+		return hand;
 	}
 
 
 
-	//3OAK
-	ArrayList<Card> firstthree = new ArrayList<Card>(dealfive.subList(0, 2));
+//	public void CountPoints() {
+//		points act as counter
+//		boolean is3OAK;
+//		boolean ispair;
+//		//get deck
+//		//count player points
+//
+//		//pairs
+//		ArrayList<Card> firsttwo = new ArrayList<Card>(hand.getHand().subList(0, 1));
+//		for(int i=0; i < firsttwo.size(); i++) {
+//			if(firsttwo.get(i).getIdentifier()==(firsttwo.get(i+1).getIdentifier())) {
+//				ispair = true;
+//			}
+//		}
+//
+//		ArrayList<Card> secondtwo = new ArrayList<Card>(hand.getHand().subList(1, 2));
+//		for(int i=0; i < secondtwo.size(); i++) {
+//			if(secondtwo.get(i).getIdentifier()==(secondtwo.get(i+1).getIdentifier())) {
+//				ispair = true;
+//			}
+//		}
+//
+//		ArrayList<Card> middletwo = new ArrayList<Card>(hand.getHand().subList(2, 3));
+//		for(int i=0 ; i < middletwo.size(); i++) {
+//			if(middletwo.get(i).getIdentifier()==(middletwo.get(i+1).getIdentifier())) {
+//				ispair = true;
+//			}
+//		}
+//		ArrayList<Card> lasttwo = new ArrayList<Card>(hand.getHand().subList(2, 3));
+//		for(int i=0 ; i < lasttwo.size(); i++) {
+//			if(lasttwo.get(i).getIdentifier()==(lasttwo.get(i+1).getIdentifier())) {
+//				ispair = true;
+//			}
+//		}
+//
+//		if(ispair = true) {
+//			points += 2;
+//		}
+//
+//
+//
+//		//3OAK
+//		ArrayList<Card> firstthree = new ArrayList<Card>(hand.getHand().subList(0, 2));
+//
+//		for(int i=0; i < firstthree.size(); i++) {
+//			if(firstthree.get(i).getIdentifier()==(firstthree.get(i+1).getIdentifier())) {
+//				is3OAK = true;
+//			}
+//
+//		}
+//
+//		ArrayList<Card> middlethree = new ArrayList<Card>(hand.getHand().subList(1, 3));
+//
+//		for(int i=0; i < middlethree.size(); i++) {
+//			if(middlethree.get(i).getIdentifier()==(middlethree.get(i+1).getIdentifier())) {
+//				is3OAK = true;
+//			}
+//		}
+//
+//		ArrayList<Card> lastthree = new ArrayList<Card>(hand.getHand().subList(2, 4));
+//
+//		for(int i=0; i < lastthree.size(); i++) {
+//			if(lastthree.get(i).getIdentifier()==(lastthree.get(i+1).getIdentifier())) {
+//				is3OAK = true;
+//			}
+//		}
+//
+//		if(is3OAK = true) {
+//			points += 6;
+//		}
 
-	for(int i=0; i < firstthree.size(); i++) {
-		if(firstthree.getCardIdentifier(i).equals(firstthree.getCardIdentifier(i+1))) {
-			is3OAK = true;
-		}
+		//Straights/Runs
+		//Run of Three
+		//		boolean isstraight;
+		//		ArrayList<Cards> firstthreecards = new ArrayList<Card>(cards.subList(0, 2));
+		//
+		//
+		//		for(int i=0; i < firstthreecards.size(); i++) {
+		//			if(firstthreecards.getPointValue(i).equals(firstthreecards.getPointValue(i+1)+1) && firstthreecards.getPointValue(i) != 9||8 ) {
+		//				isstraight = true;
+		//			}
+		//
+		//		}
+		//
+		//		ArrayList<Cards> middlethreecards = new ArrayList<Card>(cards.subList(1, 3));
+		//
+		//
+		//		for(int i=0; i < middlethreecards.size(); i++) {
+		//			if(middlethreecards.getPointValue(i).equals(middlethreecards.getPointValue(i+1)+1) && middlethreecards.getPointValue(i) != 9||8 ) {
+		//				isstraight = true;
+		//			}
+		//
+		//		}
+		//		ArrayList<Cards> lastthreecards = new ArrayList<Card>(cards.subList(1, 3));
+		//
+		//
+		//		for(int i=0; i < lastthreecards.size(); i++) {
+		//			if(lastthreecards.getPointValue(i).equals(lastthreecards.getPointValue(i+1)+1) && lastthreecards.getPointValue(i) != 9||8 ) {
+		//				isstraight = true;
+		//			}
+		//
+		//		}	
+		//		
+		//		//starting with the #8
+		//		
+		//		ArrayList<Cards> firstis8 = new ArrayList<Card>(cards.subList(0, 2));
+		//
+		//
+		//		for(int i=0; i < firstis8.size(); i++) {
+		//			if(firstis8.getPointValue(i).equals(firstis8.getPointValue(i+1)+1) && firstis8.getPointValue(i) == 8 && firstis8.getCardIdentifier(2) == JACK ) {
+		//				isstraight = true;
+		//			}
+		//
+		//		}	
+		//
+		//		ArrayList<Cards> firstis8 = new ArrayList<Card>(cards.subList(1, 3));
+		//
+		//
+		//		for(int i=0; i < firstis8.size(); i++) {
+		//			if(firstis8.getPointValue(i).equals(firstis8.getPointValue(i+1)+1) && firstis8.getPointValue(i) == 8 && firstis8.getCardIdentifier(2) == JACK ) {
+		//				isstraight = true;
+		//			}
+		//
+		//		}	
+
+
+		//Total of 15
+		//Go
+	//}
+	
+/**
+ * Get the player points	
+ * @return points
+ * 
+ */
+	
+	public int getPoints() {
+		return points;
+	}
+/**
+ * Add the player points 
+ * @param pointsToAdd
+ * 
+ */
+	public void addPoints(int pointsToAdd) {
+		points += pointsToAdd;
 
 	}
-
-	ArrayList<Card> middlethree = new ArrayList<Card>(dealfive.subList(1, 3));
-
-	for(int i=0; i < middlethree.size(); i++) {
-		if(middlethree.getCardIdentifier(i).equals(middlethree.getCardIdentifier(i+1))) {
-			is3OAK = true;
-		}
-	}
-
-	ArrayList<Card> lastthree = new ArrayList<Card>(dealfive.subList(2, 4));
-
-	for(int i=0; i < lastthree.size(); i++) {
-		if(lastthree.getCardIdentifier(i).equals(lastthree.getCardIdentifier(i+1))) {
-			is3OAK = true;
-		}
-	}
-
-	//Straights/Runs
-	//Run of Three
-	//		boolean isstraight;
-	//		ArrayList<Cards> firstthreecards = new ArrayList<Card>(cards.subList(0, 2));
-	//
-	//
-	//		for(int i=0; i < firstthreecards.size(); i++) {
-	//			if(firstthreecards.getPointValue(i).equals(firstthreecards.getPointValue(i+1)+1) && firstthreecards.getPointValue(i) != 9||8 ) {
-	//				isstraight = true;
-	//			}
-	//
-	//		}
-	//
-	//		ArrayList<Cards> middlethreecards = new ArrayList<Card>(cards.subList(1, 3));
-	//
-	//
-	//		for(int i=0; i < middlethreecards.size(); i++) {
-	//			if(middlethreecards.getPointValue(i).equals(middlethreecards.getPointValue(i+1)+1) && middlethreecards.getPointValue(i) != 9||8 ) {
-	//				isstraight = true;
-	//			}
-	//
-	//		}
-	//		ArrayList<Cards> lastthreecards = new ArrayList<Card>(cards.subList(1, 3));
-	//
-	//
-	//		for(int i=0; i < lastthreecards.size(); i++) {
-	//			if(lastthreecards.getPointValue(i).equals(lastthreecards.getPointValue(i+1)+1) && lastthreecards.getPointValue(i) != 9||8 ) {
-	//				isstraight = true;
-	//			}
-	//
-	//		}	
-	//		
-	//		//starting with the #8
-	//		
-	//		ArrayList<Cards> firstis8 = new ArrayList<Card>(cards.subList(0, 2));
-	//
-	//
-	//		for(int i=0; i < firstis8.size(); i++) {
-	//			if(firstis8.getPointValue(i).equals(firstis8.getPointValue(i+1)+1) && firstis8.getPointValue(i) == 8 && firstis8.getCardIdentifier(2) == JACK ) {
-	//				isstraight = true;
-	//			}
-	//
-	//		}	
-	//
-	//		ArrayList<Cards> firstis8 = new ArrayList<Card>(cards.subList(1, 3));
-	//
-	//
-	//		for(int i=0; i < firstis8.size(); i++) {
-	//			if(firstis8.getPointValue(i).equals(firstis8.getPointValue(i+1)+1) && firstis8.getPointValue(i) == 8 && firstis8.getCardIdentifier(2) == JACK ) {
-	//				isstraight = true;
-	//			}
-	//
-	//		}	
-
-
-	//Total of 15
-	//Go
-}
-public void PlayCard(ArrayList<Card> cards ) {
-	//if card is in player's deck, choose the card, and pick
-	//if card value > than 15 or 31 then 
-}
+	
 
 }
