2b27d1216142a4d91659193637838c9448fdaff3
Leinasaar <sleinasa@cslab05.cs.skidmore.edu>
2022-03-22 08:59:08 -0400

    Enum of eventtypes edited

--- src/java/edu/skidmore/cs326/spring2022/skribbage/common/EventType.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/common/EventType.java
@@ -1,5 +1,7 @@
 package edu.skidmore.cs326.spring2022.skribbage.common;
 
+
+
 /**
  * An enum representing all possible events. Whenever a new event type is
  * written, its associated title must be included with this enum.
@@ -11,26 +13,34 @@ public enum EventType {
     /**
      * User Login event.
      */
-    USER_LOGIN,
+    USER_LOGIN("User Login Event"),
     /**
      * User Login event with hashed password.
      */
-    USER_LOGIN_HASHED,
+    USER_LOGIN_HASHED("User Login Hashed"),
     /**
      * User login event response.
      */
-    USER_LOGIN_RESPONSE,
+    USER_LOGIN_RESPONSE(""),
     /**
      * User password change event.
      */
-    USER_CHANGE_PASSWORD,
+    USER_CHANGE_PASSWORD(""),
     /**
      * User Create account event.
      */
-    USER_CREATE_ACCOUNT,
+    USER_CREATE_ACCOUNT(""),
     /**
      * User delete account. 
      */
-    USER_DELETE_ACCOUNT,
+    USER_DELETE_ACCOUNT("");
+    
+    
+    private EventType(String name) {
+        
+    }
 
 }
+
+
+
