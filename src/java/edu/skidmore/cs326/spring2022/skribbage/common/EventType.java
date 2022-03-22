bab317918546c3ebe39a0d2921437f10ca7e02a3
Leinasaar <sleinasa@cslab16.cs.skidmore.edu>
2022-03-22 18:24:11 -0400

    EventType enum edited, logging added, javadocing added, metadata being added

--- src/java/edu/skidmore/cs326/spring2022/skribbage/common/EventType.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/common/EventType.java
@@ -1,13 +1,14 @@
 package edu.skidmore.cs326.spring2022.skribbage.common;
 
-
+import org.apache.log4j.Logger;
 
 /**
  * An enum representing all possible events. Whenever a new event type is
  * written, its associated title must be included with this enum.
  *
  * @author Alex Carney
- * Reviewed Sten Leinasaar @at March 9, 2022
+ * Reviewed Sten Leinasaar @at March 22, 2022
+ * Metadata and code to access it added by Sten Leinasaar @at March 22 2022
  */
 public enum EventType {
     /**
@@ -17,27 +18,55 @@ public enum EventType {
     /**
      * User Login event with hashed password.
      */
-    USER_LOGIN_HASHED("User Login Hashed"),
+    USER_LOGIN_HASHED("User Login Hashed Event"),
     /**
      * User login event response.
      */
-    USER_LOGIN_RESPONSE(""),
+    USER_LOGIN_RESPONSE("User Login Response Event"),
     /**
      * User password change event.
      */
-    USER_CHANGE_PASSWORD(""),
+    USER_CHANGE_PASSWORD("User Change Password Event"),
     /**
      * User Create account event.
      */
-    USER_CREATE_ACCOUNT(""),
+    USER_CREATE_ACCOUNT("User Create Account Event"),
     /**
      * User delete account. 
      */
-    USER_DELETE_ACCOUNT("");
+    USER_DELETE_ACCOUNT("User Delete Account Event");
+    
+    /**
+     * The name of the event.
+     */
+    private String name;
+    /**
+     * Logger instance for logging.
+     */
+    private static final Logger LOG;
+    
+    static {
+        LOG = Logger.getLogger(EventType.class);
+    }
     
     
-    private EventType(String name) {
+    /**
+     * 
+     * @param name
+     *          The name of the event.
+     */
+    EventType(String name) {
+        this.name = name;
         
+    }
+    /**
+     * Get the name of event.
+     * 
+     * @return The name of an event.
+     */
+    public String getName() {
+        LOG.trace("Returning a name of an event");
+        return name;
     }
 
 }
