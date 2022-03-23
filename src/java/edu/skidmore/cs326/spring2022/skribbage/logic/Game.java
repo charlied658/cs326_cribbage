2762bb03ed49a24e19d5935130223e26e85a2c33
Shriner <mshriner@csr13.cs.skidmore.edu>
2022-03-23 08:41:06 -0400

    made methods and variables public

--- src/java/edu/skidmore/cs326/spring2022/skribbage/logic/Game.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/logic/Game.java
@@ -12,24 +12,24 @@ import org.apache.log4j.Logger;
 public class Game{
 
   /** the deck of cards used to play Cribbage */
-  private static Deck theDeck = new Deck();
+  static Deck theDeck = new Deck();
 
   /**The list of players who are playing this game of Cribbage */
-  private static ArrayList <Player> playerList = new ArrayList <Player> ();
+  static ArrayList <Player> playerList = new ArrayList <Player> ();
 
   /**The total score among the players during the pegging phase of the game ( 0 <= peggingTotal <= 31)*/
-  private static int peggingTotal = 0;
+  static int peggingTotal = 0;
 
   /** the crib for the game */
-  private static ArrayList<Card> crib = new ArrayList <Card> ();
+  static ArrayList<Card> crib = new ArrayList <Card> ();
 
-  private static ArrayList<Card> peggingCards = new ArrayList <Card>();
+  static ArrayList<Card> peggingCards = new ArrayList <Card>();
 
   /**
   Initializes the ArrayList of Player objects given the number of players for this game.
   @param numPlayers is the number of players
   */
-  private static void initPlayers(int numPlayers){
+  public static void initPlayers(int numPlayers){
 
     for (int i = 0; i < numPlayers; i++){
       Player p = new Player();
@@ -38,19 +38,19 @@ public class Game{
 
   }//end of initPlayers
 
-  private static void setPeggingTotal(int pts){
+  public static void setPeggingTotal(int pts){
     peggingTotal = pts;
   }
 
-  private static Deck getDeck(){
+  public static Deck getDeck(){
     return theDeck;
   }
 
-  private static int getPeggingTotal(){
+  public static int getPeggingTotal(){
     return peggingTotal;
   }
 
-  private static ArrayList<Card> getPeggingCards(){
+  public static ArrayList<Card> getPeggingCards(){
     return peggingCards;
   }
 
@@ -58,11 +58,11 @@ public class Game{
   Returns the list of players.
   @return an ArrayList of players.
   */
-  private static ArrayList <Player> getPlayerList (){
+  public static ArrayList <Player> getPlayerList (){
     return playerList;
   }
 
-  private static ArrayList <Card> getCrib(){
+  public static ArrayList <Card> getCrib(){
     return crib;
   }
 
@@ -71,7 +71,7 @@ public class Game{
   Returns the index in playerList where the dealer is or -1.
   @return the index in playerList where the dealer is or -1 if there is no dealer.
   */
-  private static int getDealerIdx(){
+  public static int getDealerIdx(){
     for (int i = 0; i < playerList.size(); i++){
       if (playerList.get(i).isDealer == true){
         return i;
