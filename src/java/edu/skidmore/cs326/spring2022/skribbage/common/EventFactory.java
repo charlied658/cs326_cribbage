20010657e30a9c33958af641641603c8666939ad
Leinasaar <sleinasa@cslab15.cs.skidmore.edu>
2022-03-23 16:07:29 -0400

    Event Factory implementation

--- src/java/edu/skidmore/cs326/spring2022/skribbage/common/EventFactory.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/common/EventFactory.java
@@ -2,30 +2,76 @@ package edu.skidmore.cs326.spring2022.skribbage.common;
 
 import java.beans.PropertyChangeEvent;
 
+import org.apache.log4j.Logger;
+
+import edu.skidmore.cs326.spring2022.skribbage.common.events.AccountEvent;
+
 /**
  * This class creates an event of type PropertyChangeEvent
  * from the metadata given in EventType enum.
  * 
  * @author Sten Leinasaar
  */
-@SuppressWarnings("serial")
-public class EventFactory extends PropertyChangeEvent {
+public class EventFactory {
 
-    
-    
-    
-    
-    
+    /**
+     * 
+     */
+    private static final EventFactory INSTANCE;
+
+    /**
+     * 
+     */
+    private PropertyChangeEvent event;
+
+    /**
+     * Logger instance for logging.
+     */
+    private static final Logger LOG;
+
+    static {
+        LOG = Logger.getLogger(EventFactory.class);
+        INSTANCE = new EventFactory();
+    }
+
+    /**
+     * 
+     */
+    private EventFactory() {
+
+    }
+
+    /**
+     * @return Singelton instance of the factory.
+     */
+    public static synchronized EventFactory getInstance() {
+        return INSTANCE;
+    }
+
     /**
      * @param source
-     * @param propertyName
-     * @param oldValue
-     * @param newValue
+     *            Source that fired the update.
+     * @param user
+     *            User instance related to an event.
+     * @param event
+     *            Type of an event as specified from the ENUM.
+     * @return An event of type that was specified.
      */
-    public EventFactory(Object source, String propertyName, Object oldValue,
-        Object newValue) {
-        super(source, propertyName, oldValue, newValue);
-        
+    public PropertyChangeEvent createAccountEvent(Object source, User user,
+        EventType event) {
+
+        return null;
+    }
+
+    /**
+     * @param source
+     * @param user
+     * @param event
+     * @return A Cribbage event of type PropertyChangeEvent.
+     */
+    public PropertyChangeEvent createCribbageEvent(Object source, User user,
+        EventType event) {
+        return null;
     }
 
 }
