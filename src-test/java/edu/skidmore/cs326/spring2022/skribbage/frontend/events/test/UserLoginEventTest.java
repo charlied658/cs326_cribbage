dcddad7a76138c92615bda7156cdd038d6c0bf20
Leinasaar <sleinasa@cslab05.cs.skidmore.edu>
2022-03-29 08:58:01 -0400

    Minor changes

--- src-test/java/edu/skidmore/cs326/spring2022/skribbage/frontend/events/test/UserLoginEventTest.java
+++ src-test/java/edu/skidmore/cs326/spring2022/skribbage/frontend/events/test/UserLoginEventTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
-
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
