a89f05f4093aed6ef9484e1d739cb7ad756a8505
Morris <dmorris2@cslab16.cs.skidmore.edu>
2022-03-29 09:07:28 -0400

    checkstyled

---
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/logic/UserLoginResponseEvent.java
@@ -0,0 +1,28 @@
+// package edu.skidmore.cs326.spring2022.skribbage.logic;
+//
+// import edu.skidmore.cs326.spring2022.skribbage.common.*;
+//
+// /**
+// * @author Declan Morris
+// */
+// public class UserLoginResponseEvent extends AccountResponseEvent {
+//
+//   /**
+//   * Event type
+//   */
+//   private EventType eventType;
+//
+//   /**
+//   * Getter for user
+//   */
+//   public User getUser() {
+//     return user;
+//   }
+//
+//   /**
+//   * Default constructor
+//   */
+//   public UserLoginResponseEvent(Object source, User user, boolean rejected, String responseMessage) {
+//
+//   }
+// }
