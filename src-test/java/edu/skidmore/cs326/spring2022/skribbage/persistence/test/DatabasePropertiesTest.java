10b1dfff010b2de7e2242571cfde1d709d7d5502
Chantzi <nchantzi@cslab16.cs.skidmore.edu>
2022-03-30 14:38:35 -0400

    code to add to main

--- src-test/java/edu/skidmore/cs326/spring2022/skribbage/persistence/test/DatabasePropertiesTest.java
+++ src-test/java/edu/skidmore/cs326/spring2022/skribbage/persistence/test/DatabasePropertiesTest.java
@@ -16,10 +16,19 @@ import static org.junit.Assert.assertEquals;
 public class DatabasePropertiesTest {
 
 	@Test
-	public void testGetValue() {
-		assertEquals("Incorrect UserID Property","skribbage_adm", DatabaseProperties.getInstance().getValue("UserID"));
+	public void testGetUserIDValue() {
+		assertEquals("Incorrect UserID Property", "skribbage_app", DatabaseProperties.getInstance().getValue("UserID"));
 	}
-	
-	
-	
+
+	@Test
+	public void testAppPasswordGetValue() {
+		assertEquals("Incorrect AppPassword Property", "z5(DHjabY!O",
+				DatabaseProperties.getInstance().getValue("AppPassword"));
+	}
+
+	@Test
+	public void testDBUrlGetValue() {
+		assertEquals("Incorrect DBUrl Property", "jdbc:mysql://bits.monead.com:3306/skribbage",
+				DatabaseProperties.getInstance().getValue("DBUrl"));
+	}
 }
---
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/PeristenceFascade.java
@@ -0,0 +1,5 @@
+package edu.skidmore.cs326.spring2022.skribbage.persistence;
+
+public class PeristenceFascade {
+
+}
