f7d631d051821e5550276ea083bb56c0c359acd1
Rosario <rrosario@cslab17.cs.skidmore.edu>
2022-03-30 15:23:05 -0400

    new databaseManagertest

--- src-test/java/edu/skidmore/cs326/spring2022/skribbage/persistence/test/DatabaseManagerTest.java
+++ src-test/java/edu/skidmore/cs326/spring2022/skribbage/persistence/test/DatabaseManagerTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class DataBaseManager {
+public class DatabaseManagerTest {
 
 	@Before
 	public void setUp() throws Exception {
