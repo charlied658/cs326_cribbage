0a2b8206d4b1f29bf084f63fe72e58ed8d98069a
Dorjee Wangdi <dorjeewangdi@Dorjees-MacBook-Pro-2.local>
2022-03-31 12:30:58 -0400

    add constructor

--- src/java/edu/skidmore/cs326/spring2022/skribbage/logic/Hand.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/logic/Hand.java
@@ -9,14 +9,24 @@ import java.util.ArrayList;
  *         Last Edited: Dorjee
  */
 public class Hand {
-
+  
   /**
-   * hand array list.
+   * Creating variable hand to hold a players cards.
    */
-  private ArrayList<Card> hand = new ArrayList<Card>();
+  private ArrayList<Card> hand; 
+  
+  /**
+   * Constructor for Hand that initializes an ArrayList of Cards. 
+   */
+  public Hand() {
+    
+    hand = new ArrayList<Card>();
+    
+  }
+  
 
   /**
-   * Adding card to hand.
+   * Adding a card to hand. Validity of card is already checked by card class. 
    *
    * @param card
    *          to add
@@ -26,19 +36,19 @@ public class Hand {
   }
 
   /**
-   * Removing card from hand.
+   * Call to remove a card from a players hand.
    *
    * @param card
-   *          to remove
+   *          the card to be removed from hand. 
    */
   public void removeCardFromHand(Card card) {
     hand.remove(card);
   }
 
   /**
-   * Get method to return an arrayList of cards.
+   * Get method to return an arrayList of a players current cards.
    *
-   * @return Hand the players hand
+   * @return Hand the players current hand
    */
   public ArrayList<Card> getHand() {
     return hand;
