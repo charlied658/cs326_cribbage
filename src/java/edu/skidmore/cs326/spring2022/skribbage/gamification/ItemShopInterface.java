bf70b4b50d762c1f888708f441ed3dc876ce8155
Almheiri <malmhei1@cslab20.cs.skidmore.edu>
2022-03-31 18:47:33 -0400

    test classes and special card classes

--- src/java/edu/skidmore/cs326/spring2022/skribbage/gamification/ItemShopInterface.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/gamification/ItemShopInterface.java
@@ -1,13 +1,12 @@
 package edu.skidmore.cs326.spring2022.skribbage.gamification;
 
-import org.apache.log4j.Logger;
-
-import java.io.*;
 import java.util.HashMap;
+//import org.apache.log4j.Logger;
 
 /**
 *  Item Shop Interface
-*  TODO Add update methods to update player tokens after purchase and update player inventory.
+*  TODO Add update methods to update player tokens after purchase and 
+*  update player inventory.
 * @author Muaded Almheiri
 *
 */
@@ -16,34 +15,31 @@ public interface ItemShopInterface {
    /**
     * Hash map with store items being sold with their token price.
     */
-	public HashMap<String, Integer> storeItems = new HashMap<String, Integer>();
-
-	/**
-	 * Print Item name, price, and description.
-	 */
-	public void getItemInfo();
+    public HashMap<String, Integer> STORE_ITEMS = 
+	  new HashMap<String, Integer>();
 
 	/**
 	 * Buy's item if player has enough tokens.
 	 * @param playerTokens Tokens held by player.
 	 */
-	public void buyItem(int playerTokens);
+    public void buyItem(int playerTokens);
 
 	/**
 	 * Getter method for special card/item name.
 	 * @return special card/item name
 	 */
-	public String getName();
+    public String getName();
 
 	/**
 	 * Getter method for special card/item token price.
 	 * @return item token price
 	 */
-	public int getPrice();
+    public int getPrice();
 
 	/**
 	 * Getter method for special card/item description.
 	 * @return special card/item description/use.
 	 */
-	public String getDescription();
+    public String getDescription();
+    
 }
