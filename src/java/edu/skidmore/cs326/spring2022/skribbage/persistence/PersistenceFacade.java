e88dec8c9f8528900781f22302cf84affec33444
Rosario <rrosario@cslab08.cs.skidmore.edu>
2022-03-31 21:45:07 -0400

    Persistence facade

--- src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/PersistenceFacade.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/PersistenceFacade.java
@@ -13,30 +13,18 @@ public class PersistenceFacade {
 	private static final Logger LOG;
 	private static final DatabaseManager DB_Instance;
 
-	
 	static {
 		LOG = Logger.getLogger(FrontEndFactoryTemplate.class);
 	}
 
 	public String EventDBManager(EventType eventToHandle, Object[] metaData) {
 		Boolean isSuccess = true;
-		switch (eventToHandle) {
-		case "USER_LOGIN":
-		
-			return isSuccess? "success":"fail";
-		case "USER_DELETE_ACCOUNT":
-			return isSuccess? "success":"fail";
-		case "USER_CREATE_ACCOUNT":
-			return isSuccess? "success":"fail";
-		case "USER_CHANGE_PASSWORD":
-			return isSuccess? "success":"fail";
-		default:
-			return "not valid persistence event";
-		}
+
+		return isSuccess ? "success" : "fail";
 
 	}
 
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 
 	}
 }
