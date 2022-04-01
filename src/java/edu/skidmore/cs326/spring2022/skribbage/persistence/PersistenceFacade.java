838e018656f79d5f96ad10bb1284d56c7fa37c93
Irvin <iirvin@cslab09.cs.skidmore.edu>
2022-03-31 21:01:14 -0400

    Fixed compilation issues

--- src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/PersistenceFacade.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/PersistenceFacade.java
@@ -9,37 +9,34 @@ import org.apache.log4j.Logger;
 import edu.skidmore.cs326.spring2022.skribbage.common.FactoryTemplate;
 
 public class PersistenceFacade {
-	
-	
-	
 
-	private static final Logger LOG;
-	private static final DatabaseManager DB_Instance;
+    private static final Logger LOG;
 
-	
-	static {
-		LOG = Logger.getLogger(FrontEndFactoryTemplate.class);
-	}
+    private static DatabaseManager DB_Instance;
 
-	public String EventDBManager(EventType eventToHandle, Object[] metaData) {
-		Boolean isSuccess = true;
-		switch (eventToHandle) {
-		case "USER_LOGIN":
-		
-			return isSuccess? "success":"fail";
-		case "USER_DELETE_ACCOUNT":
-			return isSuccess? "success":"fail";
-		case "USER_CREATE_ACCOUNT":
-			return isSuccess? "success":"fail";
-		case "USER_CHANGE_PASSWORD":
-			return isSuccess? "success":"fail";
-		default:
-			return "not valid persistence event";
-		}
+    static {
+        LOG = Logger.getLogger(FrontEndFactoryTemplate.class);
+    }
 
-	}
+    public String EventDBManager(EventType eventToHandle, Object[] metaData) {
+        Boolean isSuccess = true;
+        switch (eventToHandle) {
+            case USER_LOGIN:
 
-	public static void main(String[] args) {
+                return isSuccess ? "success" : "fail";
+            case USER_DELETE_ACCOUNT:
+                return isSuccess ? "success" : "fail";
+            case USER_CREATE_ACCOUNT:
+                return isSuccess ? "success" : "fail";
+            case USER_CHANGE_PASSWORD:
+                return isSuccess ? "success" : "fail";
+            default:
+                return "not valid persistence event";
+        }
 
-	}
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
