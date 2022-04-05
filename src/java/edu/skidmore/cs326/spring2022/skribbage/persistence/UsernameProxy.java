fe6e1edafb86aa7fb5538d5a4270652867accd6d
Mawocha <tmawocha@cslab07.cs.skidmore.edu>
2022-04-05 09:35:42 -0400

    added beginning of code to deal with bad nicknames

---
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/UsernameProxy.java
@@ -0,0 +1,69 @@
+package edu.skidmore.cs326.spring2022.skribbage.persistence;
+
+import java.util.HashMap;
+import java.util.Properties;
+import java.io.File;
+import java.util.Scanner;
+
+
+
+public class UsernameProxy {
+	
+ 	
+ 
+ private static final String PROPERTY_FILE_NAME = "database.properties";
+
+	private HashMap badNicknames;
+	
+	private static final UsernameProxy INSTANCE;
+	
+	static {
+		INSTANCE = new UsernameProxy();
+	}
+	
+	
+	public static UsernameProxy getInstance() {
+		return INSTANCE;
+	}
+
+	
+	
+	public void parseString() {
+		String path =
+				
+		File file = new File("C:\\Users\\pankaj\\Desktop\\test.txt");
+	    Scanner sc = new Scanner(file);
+	 
+	    while (sc.hasNextLine())
+	      System.out.println(sc.nextLine());
+		
+		
+		
+		
+		
+	}
+	
+	public UsernameProxy() {
+		
+		
+		Properties properties = new Properties();
+		ClassLoader
+		
+		while( placeholder.hasNext()   ) {
+		
+			badNicknames.put
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+
+	
+
+}
