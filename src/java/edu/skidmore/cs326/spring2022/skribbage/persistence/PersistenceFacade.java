2f9e001b6af74caa8be1b7e6f4064fafb013d9f9
Rosario <rrosario@cslab13.cs.skidmore.edu>
2022-04-06 08:05:50 -0400

    small change

--- src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/PersistenceFacade.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/PersistenceFacade.java
@@ -1,44 +1,16 @@
 package edu.skidmore.cs326.spring2022.skribbage.persistence;
 
-//import edu.skidmore.cs326.spring2022.skribbage.frontend.events.*;
 import edu.skidmore.cs326.spring2022.skribbage.common.EventType;
-//import java.beans.PropertyChangeEvent;
 
-//import org.apache.log4j.Logger;
+
+
 
 /**
  * Will contain the methods for an event listener and call the methods in the DatabaseManager
  */
 public class PersistenceFacade {
-
-//<<<<<<< HEAD
-	//private static final Logger LOG;
-	//private static final DatabaseManager DB_Instance;
 	
-
-//	static {
-//		LOG = Logger.getLogger(FrontEndFactoryTemplate.class);
-//	}
-
-//	public String EventDBManager(EventType eventToHandle, Object[] metaData) {
-//		Boolean isSuccess = true;
-//
-//		return isSuccess ? "success" : "fail";
-//
-//	}
-
-//	 public static void main(String[] args) {
-//
-//	}
-//=======
-//    private static final Logger LOG;
-//
-//    private static DatabaseManager DB_Instance;
-//
-//    static {
-//        LOG = Logger.getLogger(FrontEndFactoryTemplate.class);
-//    }
-
+	
 	/**
 	 * Will listen for the events and manage what method is being called
 	 * @param eventToHandle the event that is being passed
@@ -63,9 +35,4 @@ public class PersistenceFacade {
         }
 
     }
-
-//    public static void main(String[] args) {
-//
-//    }
-//>>>>>>> 01d6bd53f021b02c7735798961a7c627dd271523
 }
