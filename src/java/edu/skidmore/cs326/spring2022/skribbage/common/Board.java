555dc71864f7d54eaddfe8a71bb902210d2c2fee
Leinasaar <sleinasa@cslab11.cs.skidmore.edu>
2022-04-07 15:29:51 -0400

    Edited board class

--- src/java/edu/skidmore/cs326/spring2022/skribbage/common/Board.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/common/Board.java
@@ -18,10 +18,13 @@ public class Board {
      * Board image attribute.
      */
     private Image board;
+
     /**
      * Array of pegs.
      */
     // private Peg[] pegs;
+
+    private Spot[] grid;
 
     static {
         LOG = Logger.getLogger(Board.class);
@@ -31,19 +34,29 @@ public class Board {
      * Constructor.
      */
     public Board() {
+        LOG.info("Constructor of a Board class reached.");
+    }
+
+    /**
+     * @return grid consisting of spots.
+     */
+    public Spot[] getGrid() {
+        LOG.trace("Returning a grid as an array of spots.");
+        return grid;
 
     }
 
-    // public Spot[] getGrid() {
-    //
-    // }
     // public Peg[] getPegs() {
     //
     // }
+
     /**
-     * @return A spot.
+     * This method checks the game grid and returns the positions with pegs.
+     * 
+     * @return An array of occupied spots of type Spot
      */
     public Spot[] getOccupiedSpots() {
+        LOG.trace("Returning occupied spots as an array of spots.");
         return null;
         //
     }
@@ -51,26 +64,41 @@ public class Board {
     // public Peg getSpot(Peg p) {
     //
     // }
+
     /**
-     * @return spot.
+     * This method assigns a random spot to be a prize spot and returns the grid
+     * location of that spot. The type of that spot will be changed to PRIZE.
+     * 
+     * @return a priceSpot location of type spot.
      */
     public Spot assignPrizeSpot() {
+        LOG.trace("Returning a location of a prizeSpot.");
         return null;
 
     }
 
     /**
-     * @return spot.
+     * This method assigns random 8 spots to be a battle spot and returns the
+     * grid location of that spot. The type of that spot will be changed to
+     * BATTLE.
+     * 
+     * @return a battleSpot location of type spot.
      */
-    public Spot assignBattleSpot() {
+    public Spot[] assignBattleSpot() {
+        LOG.trace(
+            "Returning a location of a battleSpots as an array of spots.");
         return null;
 
     }
 
     /**
-     * @return spot.
+     * This method assigns a random spot to be a jump spot and returns the grid
+     * location of that spot. The type of that spot will be change to JUMP.
+     * 
+     * @return a jumpSpot location of type spot.
      */
     public Spot assignJumpSpot() {
+        LOG.trace("Returning a location of a jumpSpot.");
         return null;
 
     }
