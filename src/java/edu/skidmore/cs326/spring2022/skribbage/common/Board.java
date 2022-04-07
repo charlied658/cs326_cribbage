e69ec1480bf9192de36d80f9afb37f587d24385a
Leinasaar <sleinasa@cslab03.cs.skidmore.edu>
2022-04-07 10:35:03 -0400

    Board class edited

--- src/java/edu/skidmore/cs326/spring2022/skribbage/common/Board.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/common/Board.java
@@ -1,11 +1,11 @@
 package edu.skidmore.cs326.spring2022.skribbage.common;
+
 import org.apache.log4j.Logger;
 
 import us.daveread.edu.graphics.shape.impl.Image;
+
 /**
- * 
  * @author Sten Leinasaar
- *
  */
 public class Board {
 
@@ -13,6 +13,7 @@ public class Board {
      * Logger instance for logging.
      */
     private static final Logger LOG;
+
     /**
      * Board image attribute.
      */
@@ -20,39 +21,58 @@ public class Board {
     /**
      * Array of pegs.
      */
-    //private Peg[] pegs;
-    
+    // private Peg[] pegs;
+
     static {
         LOG = Logger.getLogger(Board.class);
     }
+
     /**
      * Constructor.
      */
     public Board() {
-        
+
     }
-    
-//    public Spot[] getGrid() {
-//        
-//    }
-//    public Peg[] getPegs() {
-//        
-//    }
-//    public Spot[] getOccupiedSpots() {
-//        
-//    }
-//    public Peg getSpot(Peg p) {
-//        
-//    }
-//    public Spot assignPrizeSpot() {
-//        
-//    }
-//    public Spot assignBattleSpot() {
-//        
-//    }
-//    public Spot assignJumpSpot() {
-//        
-//    }
-    
-    
+
+    // public Spot[] getGrid() {
+    //
+    // }
+    // public Peg[] getPegs() {
+    //
+    // }
+    /**
+     * @return A spot.
+     */
+    public Spot[] getOccupiedSpots() {
+        return null;
+        //
+    }
+
+    // public Peg getSpot(Peg p) {
+    //
+    // }
+    /**
+     * @return spot.
+     */
+    public Spot assignPrizeSpot() {
+        return null;
+
+    }
+
+    /**
+     * @return spot.
+     */
+    public Spot assignBattleSpot() {
+        return null;
+
+    }
+
+    /**
+     * @return spot.
+     */
+    public Spot assignJumpSpot() {
+        return null;
+
+    }
+
 }
