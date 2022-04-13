6a565cb14e891eee85fd0f1753b0f7bd6158b2f0
Rosario <rrosario@cslab13.cs.skidmore.edu>
2022-04-13 08:09:39 -0400

    facade merge

--- src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/PersistenceFacade.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/PersistenceFacade.java
@@ -2,7 +2,6 @@ package edu.skidmore.cs326.spring2022.skribbage.persistence;
 
 import org.apache.log4j.Logger;
 
-import edu.skidmore.cs326.spring2022.skribbage.common.EventType;
 import edu.skidmore.cs326.spring2022.skribbage.common.Password;
 import edu.skidmore.cs326.spring2022.skribbage.common.User;
 import edu.skidmore.cs326.spring2022.skribbage.logic.Game;
@@ -35,6 +34,14 @@ public final class PersistenceFacade implements UserManagement, GameManagement{
 	static {
 		LOG = Logger.getLogger(PersistenceFacade.class);
 		INSTANCE = new PersistenceFacade();
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static PersistenceFacade getInstance() {
+		return INSTANCE;
 	}
 	
 	/**
