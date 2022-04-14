1b7dca0c12037de23d681a93415f7fffd5d2e192
Rosario <rrosario@cslab11.cs.skidmore.edu>
2022-04-13 21:09:13 -0400

    Facadetest and databasemanagertest

--- src-test/java/edu/skidmore/cs326/spring2022/skribbage/persistence/test/FacadeTest.java
+++ src-test/java/edu/skidmore/cs326/spring2022/skribbage/persistence/test/FacadeTest.java
@@ -5,14 +5,42 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.skidmore.cs326.spring2022.skribbage.common.Password;
+import edu.skidmore.cs326.spring2022.skribbage.common.User;
+import edu.skidmore.cs326.spring2022.skribbage.persistence.DatabaseManager;
+import edu.skidmore.cs326.spring2022.skribbage.persistence.PersistenceFacade;
+
 /**
  * @author Ricardo Rosario
  *         Last Edited: By Ricardo, April 13, 2022
  */
 public class FacadeTest {
-
+	
+	/**
+	 * The Test instatnce of Facade
+	 * Needed to create 2 users
+     * There is 1 correct user with the correct password and correct username
+     * The other user is with the wrong password and wrong username
+     * that is not in the database
+	 */
+	private PersistenceFacade facadeinstanace;
+	private User wrongTestUser;
+    private Password wrongTestPassword;
+    private User correctTestUser;
+    private Password correctTestPassword;
+	
+	/**
+	 * Sets up the default testing setting before everytest.
+	 * 
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
+		facadeinstanace = new PersistenceFacade();
+        wrongTestUser = new User("DJKhaled.com", "Khaled", "AnotherOne", null);
+        wrongTestPassword = new Password("AnotherOne");
+        correctTestUser = new User("nchantzi@skidmore.edu", "nchantzi", "ILoveSQL", null);
+        correctTestPassword = new Password("ILoveSQL");
 	}
 
 	@Test
