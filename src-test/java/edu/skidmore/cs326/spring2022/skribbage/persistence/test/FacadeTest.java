36ec4a73e1063f653f2dea42b75b8dad825e9d3d
Rosario <rrosario@cslab11.cs.skidmore.edu>
2022-04-13 20:36:08 -0400

    facade test

---
+++ src-test/java/edu/skidmore/cs326/spring2022/skribbage/persistence/test/FacadeTest.java
@@ -0,0 +1,23 @@
+package edu.skidmore.cs326.spring2022.skribbage.persistence.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Ricardo Rosario
+ *         Last Edited: By Ricardo, April 13, 2022
+ */
+public class FacadeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
