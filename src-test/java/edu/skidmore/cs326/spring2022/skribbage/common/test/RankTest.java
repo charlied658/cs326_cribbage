88f2a4b5c150e55bf8818f05d903b8f30e784bd3
Morris <dmorris2@cslab17.cs.skidmore.edu>
2022-04-14 14:42:59 -0400

    Added testing for Suit and Rank classes

---
+++ src-test/java/edu/skidmore/cs326/spring2022/skribbage/common/test/RankTest.java
@@ -0,0 +1,52 @@
+package edu.skidmore.cs326.spring2022.skribbage.common.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.skidmore.cs326.spring2022.skribbage.common.Rank;
+
+/**
+ * Test class for Rank java bean.
+ * 
+ * @author Declan Morris
+ */
+public class RankTest {
+    /**
+     * Attribute to house the test instance to run tests on.
+     */
+    private Rank testInstance;
+
+    /**
+     * Instantiate the test instance.
+     */
+    @Before
+    public void setup() {
+        testInstance = Rank.JACK;
+    }
+
+    /**
+     * Test the getName method.
+     */
+    @Test
+    public void testGetName() {
+        assertEquals(testInstance.getName(), "Jack");
+    }
+    
+    /**
+     * Test the getSymbol method.
+     */
+    @Test
+    public void testGetSymbol() {
+        assertEquals(testInstance.getSymbol(), "J");
+    }
+    
+    /**
+     * Test the getPointValue method.
+     */
+    @Test
+    public void testGetPointValue() {
+        assertEquals(testInstance.getPointValue(), 10);
+    }
+}
