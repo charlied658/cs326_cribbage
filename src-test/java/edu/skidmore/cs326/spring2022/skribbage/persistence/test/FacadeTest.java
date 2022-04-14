dd0e7e71fc53879398e56e014208b93a5b64afd9
Rosario <rrosario@cslab11.cs.skidmore.edu>
2022-04-13 21:31:16 -0400

    tests

--- src-test/java/edu/skidmore/cs326/spring2022/skribbage/persistence/test/FacadeTest.java
+++ src-test/java/edu/skidmore/cs326/spring2022/skribbage/persistence/test/FacadeTest.java
@@ -11,24 +11,21 @@ import edu.skidmore.cs326.spring2022.skribbage.persistence.DatabaseManager;
 import edu.skidmore.cs326.spring2022.skribbage.persistence.PersistenceFacade;
 
 /**
- * @author Ricardo Rosario
- *         Last Edited: By Ricardo, April 13, 2022
+ * @author Ricardo Rosario Last Edited: By Ricardo, April 13, 2022
  */
 public class FacadeTest {
-	
+
 	/**
-	 * The Test instatnce of Facade
-	 * Needed to create 2 users
-     * There is 1 correct user with the correct password and correct username
-     * The other user is with the wrong password and wrong username
-     * that is not in the database
+	 * The Test instatnce of Facade Needed to create 2 users There is 1 correct user
+	 * with the correct password and correct username The other user is with the
+	 * wrong password and wrong username that is not in the database
 	 */
 	private PersistenceFacade facadeinstanace;
 	private User wrongTestUser;
-    private Password wrongTestPassword;
-    private User correctTestUser;
-    private Password correctTestPassword;
-	
+	private Password wrongTestPassword;
+	private User correctTestUser;
+	private Password correctTestPassword;
+
 	/**
 	 * Sets up the default testing setting before everytest.
 	 * 
@@ -37,15 +34,45 @@ public class FacadeTest {
 	@Before
 	public void setUp() throws Exception {
 		facadeinstanace = new PersistenceFacade();
-        wrongTestUser = new User("DJKhaled.com", "Khaled", "AnotherOne", null);
-        wrongTestPassword = new Password("AnotherOne");
-        correctTestUser = new User("nchantzi@skidmore.edu", "nchantzi", "ILoveSQL", null);
-        correctTestPassword = new Password("ILoveSQL");
+		wrongTestUser = new User("DJKhaled.com", "Khaled", "AnotherOne", null);
+		wrongTestPassword = new Password("AnotherOne");
+		correctTestUser = new User("nchantzi@skidmore.edu", "nchantzi", "ILoveSQL", null);
+		correctTestPassword = new Password("ILoveSQL");
 	}
 
+	/**
+	 * Testing when the userDelete corrects with the correct user
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void userDeleteTestCorrect() {
+		boolean iscorrect = facadeinstanace.userDelete(correctTestUser);
+		assertEquals(true, iscorrect);
 	}
+
+//	@Test
+//	public void userDeleteTestFail() {
+//		boolean iscorrect = facadeinstanace.userDelete(wrongTestUser);
+//		assertEquals(false, iscorrect);
+//	}
+
+	/**
+	 * Testing when the user wants to change their password Passing in the user, the
+	 * current pass and the new password
+	 */
+	@Test
+	public void passwordChangeCorrect() {
+		boolean iscorrect = facadeinstanace.passwordChange(correctTestUser, correctTestPassword, wrongTestPassword);
+		assertEquals(true, iscorrect);
+	}
+	
+	/**
+	 * Testing when the user wants to change their password Passing in the user, the
+	 * current pass and the new password however it fails
+	 */
+//	@Test
+//	public void passwordChangeFail() {
+//		boolean iscorrect = facadeinstanace.passwordChange(correctTestUser, correctTestPassword, wrongTestPassword);
+//		assertEquals(true, iscorrect);
+//	}
 
 }
