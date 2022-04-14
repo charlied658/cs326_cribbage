54c09d56b1be3cc0ff8281d4aed05e75bcf9146b
ACarney <paridisedwarfhigh@hotmail.com>
2022-04-14 19:18:15 -0400

    Alex code review tests

--- src-test/java/edu/skidmore/cs326/spring2022/skribbage/common/test/PasswordTest.java
+++ src-test/java/edu/skidmore/cs326/spring2022/skribbage/common/test/PasswordTest.java
@@ -6,8 +6,12 @@ import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static junit.framework.TestCase.*;
 
+/**
+ * Simple class for testing the user bean.
+ * @author Alex Carney
+ */
 public class PasswordTest {
 
     /**
@@ -36,7 +40,7 @@ public class PasswordTest {
     }
 
     /**
-     * Method to set up testing env
+     * Method to set up testing env.
      */
     @Before
     public void setup() {
@@ -47,24 +51,39 @@ public class PasswordTest {
 
     }
 
+    /**
+     * Tests getting password value.
+     */
     @Test
     public void testPasswordValue() {
         assertNotNull(testInstance);
         assertEquals(testInstance.getPasswordValue(), TEST_PASSWORD_VALUE);
     }
 
+    /**
+     * Tests resetting password value.
+     */
     @Test
     public void testResettingPasswordValue() {
-        testInstance.setPasswordValue(TEST_PASSWORD_VALUE + TEST_PASSWORD_VALUE);
-        assertEquals(testInstance.getPasswordValue(), TEST_PASSWORD_VALUE + TEST_PASSWORD_VALUE);
+        testInstance.setPasswordValue(TEST_PASSWORD_VALUE
+            + TEST_PASSWORD_VALUE);
+        assertEquals(testInstance.getPasswordValue(), TEST_PASSWORD_VALUE
+            + TEST_PASSWORD_VALUE);
     }
 
+    /**
+     * Tests hashing password. (or at least setting isHashed to true)
+     */
     @Test
     public void testHashingPassword() {
         testInstance.setHashed(true);
         assertTrue(testInstance.isHashed());
     }
 
+    /**
+     * Test adding salted password. Blocked by Logic tier as
+     * of 4/14.
+     */
     @Test
     public void testAddingSaltedPassword() {
         //TODO: Why is password hasher all protected??
