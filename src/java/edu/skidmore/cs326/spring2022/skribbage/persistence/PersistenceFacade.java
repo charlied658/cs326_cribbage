e87227c7c0cd0ac9ea65521de82965a36d8a6370
Rosario <rrosario@cslab09.cs.skidmore.edu>
2022-04-14 23:23:34 -0400

    createuserchange

--- src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/PersistenceFacade.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/PersistenceFacade.java
@@ -64,8 +64,6 @@ public final class PersistenceFacade implements UserManagement, GameManagement, 
 		
 		String usernamge = userToCreate.getUserName();
 		String passwordtemp = password.getPasswordValue();
-		System.out.println(usernamge);
-		System.out.println(passwordtemp);
 		
 		dm.createUser(usernamge, passwordtemp);
 		return true;
