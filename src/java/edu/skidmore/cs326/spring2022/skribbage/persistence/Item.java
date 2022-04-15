e5c8f3b24fe6140db2881b85f7aa240af6a3d331
Rosario <rrosario@cslab09.cs.skidmore.edu>
2022-04-14 22:32:13 -0400

    facadetest

--- src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/Item.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/Item.java
@@ -1,39 +1,29 @@
 package edu.skidmore.cs326.spring2022.skribbage.persistence;
 
 public class Item {
-	
-	
+
 	private ItemTypes itemType;
-	
+
 	private int quantityHeld;
-	
 
 	public ItemTypes getItemType() {
 		return itemType;
 	}
 
-
-
 	public void setItemType(ItemTypes itemType) {
 		this.itemType = itemType;
 	}
-
-
 
 	public int getQuantityHeld() {
 		return quantityHeld;
 	}
 
-
-
 	public void setQuantityHeld(int quantityHeld) {
 		this.quantityHeld = quantityHeld;
 	}
 
-
-
 	public String toString() {
-		return "item type: " + itemType + "\nquantity held: " + quantityHeld ;
+		return "item type: " + itemType + "\nquantity held: " + quantityHeld;
 	}
 
 }
--- src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/ItemTypes.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/ItemTypes.java
@@ -2,17 +2,12 @@ package edu.skidmore.cs326.spring2022.skribbage.persistence;
 
 public enum ItemTypes {
 
-	
-	PARTY_HAT(001),
-	BIRTHDAY_CAKE(002),
-	MAGNIFYING_GLASS(003);
-	
-	
+	PARTY_HAT(001), BIRTHDAY_CAKE(002), MAGNIFYING_GLASS(003);
+
 	private int itemId;
-	
-	private ItemTypes(int theItemId){
+
+	private ItemTypes(int theItemId) {
 		this.itemId = theItemId;
 	}
-	
-	
+
 }
