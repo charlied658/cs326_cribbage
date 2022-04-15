fc65e483b2522cebb2c7e0c7bf5cf64add1673fc
Irvin <iirvin@cslab03.cs.skidmore.edu>
2022-04-14 22:09:18 -0400

    commenting out snippet.java

--- src/java/edu/skidmore/cs326/spring2022/skribbage/Snippet.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/Snippet.java
@@ -2,7 +2,7 @@ package edu.skidmore.cs326.spring2022.skribbage;
 
 public class Snippet {
 	public static void main(String[] args) {
-		ssh cs326mysql@bits.monead.com
+		//ssh cs326mysql@bits.monead.com;
 	}
 }
 
