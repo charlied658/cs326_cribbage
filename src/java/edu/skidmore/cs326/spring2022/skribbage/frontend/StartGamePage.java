b963d3fc25f6aef6ac22ed375886d920f030559b
Davidson <cdavidso@cslab16.cs.skidmore.edu>
2022-04-15 22:08:12 -0400

    Add board functionality

--- src/java/edu/skidmore/cs326/spring2022/skribbage/frontend/StartGamePage.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/frontend/StartGamePage.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.util.Arrays;
+//import java.util.Random;
 
 import org.apache.log4j.Logger;
 
@@ -13,6 +14,7 @@ import edu.skidmore.cs326.spring2022.skribbage.common.Spot;
 import us.daveread.edu.graphics.shape.Drawable;
 import us.daveread.edu.graphics.shape.impl.Image;
 import us.daveread.edu.graphics.shape.impl.Rectangle;
+import us.daveread.edu.graphics.shape.impl.Circle;
 import us.daveread.edu.graphics.shape.impl.Text;
 import us.daveread.edu.graphics.surface.DrawingSurface;
 import us.daveread.edu.graphics.surface.MainFrame;
@@ -76,10 +78,20 @@ public class StartGamePage extends DrawingSurface {
     // private User player2;
 
     /**
+     * Board to hold the spots.
+     */
+    private Rectangle board;
+    
+    /**
      * gameArea - space to hold the game playing area.
      */
     private Rectangle gameArea;
 
+    /**
+     * Visual representation of the spots on the board.
+     */
+    private Circle[][] spotRenderer;
+    
     /**
      * returnHome -Text variable to represent back button.
      */
@@ -106,6 +118,76 @@ public class StartGamePage extends DrawingSurface {
         LOG.trace("StartGamePage constructor");
         startGamePage = new MainFrame(this, "Start Game Page", 900, 900, false);
         setup();
+        createSpots();
+        //animateSpots();
+    }
+    
+    /**
+     * Renders the spots on the board.
+     */
+    public void createSpots() {
+        spotRenderer = new Circle[120][3];
+        for (int i = 0; i < spotRenderer.length; i++) {
+            for (int j = 0; j < spotRenderer[0].length; j++) {
+                spotRenderer[i][j] = new Circle(
+                    new Point(60 + j * 20 + (i / 30) * 80,
+                        650 - (i % 30) * 20), 16,
+                    Color.black, Color.black);
+                add(spotRenderer[i][j]);
+                //System.out.println("Adding spot [" + i + "][" + j + "]");
+            }
+        }
+    }
+    
+    /**
+     * Test animation of spots on board.
+     * @author Charlie Davidson
+     */
+    public void animateSpots() {
+        
+        //Random rand = new Random();
+        
+        int redPosition = 0;
+        int bluePosition = 0;
+        int yellowPosition = 0;
+        
+        
+        while (true) {
+            for (int i = 0; i < spotRenderer.length; i++) {
+                if (redPosition == i) {
+                    spotRenderer[i][0].setFillColor(Color.red);
+                } else {
+                    spotRenderer[i][0].setFillColor(Color.black);
+                }
+                
+                if (bluePosition == i) {
+                    spotRenderer[i][1].setFillColor(Color.blue);
+                } else {
+                    spotRenderer[i][1].setFillColor(Color.black);
+                }
+                
+                if (yellowPosition == i) {
+                    spotRenderer[i][2].setFillColor(Color.yellow);
+                } else {
+                    spotRenderer[i][2].setFillColor(Color.black);
+                }
+                
+            }
+            //redPosition = (redPosition + rand.nextInt(5)) % 120;
+            //bluePosition = (bluePosition + rand.nextInt(5)) % 120;
+            //yellowPosition = (yellowPosition + rand.nextInt(5)) % 120;
+            
+            redPosition = (redPosition + 1) % 120;
+            bluePosition = (bluePosition + 1) % 120;
+            yellowPosition = (yellowPosition + 1) % 120;
+            
+            try {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 
     /**
@@ -113,10 +195,12 @@ public class StartGamePage extends DrawingSurface {
      */
     public void setup() {
         LOG.trace("setup method in StartGamePage.java");
-        boardImage = new Image("newboard.png", new Point(40, 65), 1.5, null);
+        //boardImage = new Image("newboard.png", new Point(40, 65), 1.5, null);
+        board = new Rectangle(new Point(35, 50), new Dimension(350, 700),
+            Color.black, new Color(180, 110, 30));
         gameArea = new Rectangle(new Point(25, 40),
             new Dimension(850, 800), Color.black, Color.green);
-        beginGame = new Text("Start", new Point(375, 400), 20, Color.black,
+        beginGame = new Text("Start", new Point(35, 880), 20, Color.black,
             Color.blue);
         cardDeck = new Image("card.jpg", new Point(500, 315), .6, null);
         player1Score = new Text("temp player 1:", new Point(35, 790), 20,
@@ -131,7 +215,8 @@ public class StartGamePage extends DrawingSurface {
         add(player2Score);
         add(player1Score);
         add(returnHome);
-        add(boardImage);
+        add(board);
+        //add(boardImage);
         createGrid();
     }
 
@@ -162,6 +247,7 @@ public class StartGamePage extends DrawingSurface {
         LOG.trace("drawableMouseClick method in StartGamepage.java");
         if (e == beginGame) {
             // start game
+            animateSpots();
             LOG.trace("Going to start the game");
         } else if (e == returnHome) {
             navPage = NavigationPageManager.getInstance().getNavPage();
