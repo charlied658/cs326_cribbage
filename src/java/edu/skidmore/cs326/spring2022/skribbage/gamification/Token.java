210163d97459e558bb2302cd8d6a589be4ef5f22
Ratsimbazafy <jratsimb@cslab21.cs.skidmore.edu>
2022-04-18 08:47:13 -0400

    fixed checkstyle error

--- src/java/edu/skidmore/cs326/spring2022/skribbage/gamification/Token.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/gamification/Token.java
@@ -1,17 +1,14 @@
 package edu.skidmore.cs326.spring2022.skribbage.gamification;
 
 /**
- * 
  * @author jaroderatsimbazafy.
- *
- *Token Class, currency of the game.
- *
+ *         Token Class, currency of the game.
  */
 public class Token {
-	
-	/**
-	 * Constructor method.
-	 */
-	public Token() {
-	}
+
+    /**
+     * Constructor method.
+     */
+    public Token() {
+    }
 }
