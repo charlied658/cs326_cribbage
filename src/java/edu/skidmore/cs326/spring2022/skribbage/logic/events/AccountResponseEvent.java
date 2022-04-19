c838fd1066bc2c78f7453f6e6b02d23f27f5d930
Morris <dmorris2@cslab17.cs.skidmore.edu>
2022-04-19 18:34:35 -0400

    Added UserValidationResponse classes

--- src/java/edu/skidmore/cs326/spring2022/skribbage/logic/events/AccountResponseEvent.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/logic/events/AccountResponseEvent.java
@@ -15,7 +15,7 @@ public abstract class AccountResponseEvent extends PropertyChangeEvent {
      * Contains String message and boolean rejection status sent with the event.
      */
 
-    protected LoginResponse loginResponse;
+    protected AccountResponse accountResponse;
 
     /**
      * The user associated with the event.
@@ -36,10 +36,10 @@ public abstract class AccountResponseEvent extends PropertyChangeEvent {
      */
     protected AccountResponseEvent(Object source, EventType eventType,
         User user,
-        LoginResponse loginResponse) {
+        AccountResponse loginResponse) {
         super(source, eventType.toString(), null, null);
         this.user = user;
-        this.loginResponse = loginResponse;
+        this.accountResponse = loginResponse;
     }
 
     /**
@@ -63,5 +63,5 @@ public abstract class AccountResponseEvent extends PropertyChangeEvent {
      * 
      * @return loginResponse
      */
-    public abstract LoginResponse getLoginResponse();
+    public abstract AccountResponse getAccountResponse();
 }
