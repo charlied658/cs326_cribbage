8cd41cc17bc5f7c83c38ec35c5eaef9d1a7fd7fb
Chantzi <nchantzi@cslab11.cs.skidmore.edu>
2022-04-20 09:03:19 -0400

    updated code

--- src-test/java/edu/skidmore/cs326/spring2022/skribbage/persistence/test/DatabaseManagerTest.java
+++ src-test/java/edu/skidmore/cs326/spring2022/skribbage/persistence/test/DatabaseManagerTest.java
@@ -15,7 +15,6 @@ import edu.skidmore.cs326.spring2022.skribbage.persistence.DatabaseManager;
  */
 public class DatabaseManagerTest {
 
-<<<<<<< HEAD
 	/**
 	 * The Test instance of databasemanager. Needed to create 2 users There is 1
 	 * correct user with the correct password and correct username The other user is
@@ -63,9 +62,9 @@ public class DatabaseManagerTest {
 		databaseinstance = new DatabaseManager();
 		existentUsername = new String("nchantzi");
 		nonExistentUsername = new String("jbrunsta");
-		wrongTestUser = new User("DJKhaled.com", "Khaled", new Password("AnotherOne"), null);
+		//wrongTestUser = new User("DJKhaled.com", "Khaled", null);
 		wrongTestPassword = new Password("AnotherOne");
-		correctTestUser = new User("nchantzi@skidmore.edu", "nchantzi", new Password("ILoveSQL"), null);
+		//correctTestUser = new User("nchantzi@skidmore.edu", "nchantzi", null);
 		correctTestPassword = new Password("ILoveSQL");
 	}
 
@@ -126,95 +125,6 @@ public class DatabaseManagerTest {
 	// String cointest = databaseinstance.walletQuery(420);
 	// assertEquals(cointest, "Account not found");
 	// }
-=======
-    /**
-     * The Test instance of databasemanager. Needed to create 2 users There is 1
-     * correct user with the correct password and correct username The other
-     * user is
-     * with the wrong password and wrong username that is not in the database
-     */
-    private DatabaseManager databaseinstance;
 
-    /**
-     * wronttestuser.
-     */
-    private User wrongTestUser;
-
-    /**
-     * wrongtestpassword.
-     */
-    private Password wrongTestPassword;
-
-    /**
-     * correctTestuser.
-     */
-    private User correctTestUser;
-
-    /**
-     * correctestpassword.
-     */
-    private Password correctTestPassword;
-
-    /**
-     * Sets up the default testing setting before everytest.
-     * 
-     * @throws Exception
-     */
-    @Before
-    public void setUp() throws Exception {
-        databaseinstance = new DatabaseManager();
-        wrongTestUser = new User("DJKhaled@hotmail.com",
-            "Khaled", UserRole.UNAUTHORIZED);
-        wrongTestPassword = new Password("AnotherOne");
-        correctTestUser =
-            new User("nchantzi@skidmore.edu", "nchantzi",
-                UserRole.UNAUTHORIZED);
-        correctTestPassword = new Password("ILoveSQL");
-    }
-
-    /**
-     * Testing when the userAuthenticate fails with wrong username and password.
-     */
-    @Test
-    public void userAuthenticatefail() {
-        boolean verification =
-            databaseinstance.userAuthenticate(wrongTestUser, wrongTestPassword);
-        assertEquals(false, verification);
-    }
-
-    /**
-     * Testing when the userAuthenticate works with correct username and
-     * password.
-     */
-    @Test
-    public void userAuthenticateSuccess() {
-        Boolean verification =
-            databaseinstance.userAuthenticate(correctTestUser,
-                correctTestPassword);
-        assertEquals(true, verification);
-    }
-
-    /**
-     * Testing when asking the inventoryQuery for coin count is successful.
-     */// @Test
-       // public void inventoryQuerysuccess() {
-       // String cointest = databaseinstance.walletQuery(325);
-       // assertEquals(cointest, "player coin value: 100000");
-       // }
-       // @Test
-       // public void inventoryQuerysuccess() {
-       // String cointest = databaseinstance.walletQuery(325);
-       // assertEquals(cointest, "player coin value: 100000");
-       // }
-
-    /**
-     * Testing when asking the inventoryQuery for coin count fails.
-     */
-    // @Test
-    // public void inventoryQueryfails() {
-    // String cointest = databaseinstance.walletQuery(420);
-    // assertEquals(cointest, "Account not found");
-    // }
->>>>>>> 03eb7fe9ec421a84fbd712db0977b1f53d5f5e19
 
 }
