d761bfbad1c2ed6a3afd5b716df70783a7c89b19
Rosario <rrosario@cslab13.cs.skidmore.edu>
2022-04-25 08:33:25 -0400

    facade change pass

--- src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/UserManagement.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/UserManagement.java
@@ -36,8 +36,7 @@ public interface UserManagement {
      * @param newPassword
      * @return the password.
      */
-    boolean passwordChange(User userToUpdate, Password currentPassword,
-        Password newPassword);
+    boolean passwordChange(User userToUpdate, Password newPassword);
 
     /**
      * userChange.
@@ -73,11 +72,12 @@ public interface UserManagement {
      * @return boolean depending if the user name exists
      */
     boolean userNameExists(User user);
-    
+
     /**
      * Get the user's password Base64-encoded salt.
      * 
-     * @param user The user's whose password salt is to be obtained
+     * @param user
+     *            The user's whose password salt is to be obtained
      * @return The password salt or null if the user's account is not found
      */
     String getUserSaltBase64(User user);
