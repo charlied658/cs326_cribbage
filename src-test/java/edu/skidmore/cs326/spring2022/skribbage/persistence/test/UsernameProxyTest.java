9e0044eefd13caea14ebf06b8ca8d346050d0065
Mawocha <tmawocha@cslab14.cs.skidmore.edu>
2022-04-26 17:22:08 -0400

    beginning of UsernameProxyTest file:

---
+++ src-test/java/edu/skidmore/cs326/spring2022/skribbage/persistence/test/UsernameProxyTest.java
@@ -0,0 +1,19 @@
+package edu.skidmore.cs326.spring2022.skribbage.persistence.test;
+import edu.skidmore.cs326.spring2022.skribbage.persistence.UsernameProxy;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UsernameProxyTest {
+	
+	private UsernameProxy instance = new UsernameProxy();
+	
+	@Test
+	public void usernameCheckTest() {
+		
+	}
+
+}
