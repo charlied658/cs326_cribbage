ad3c8bbafe6440ca76c81594d184805ee5545cd7
Mawocha <tmawocha@cslab14.cs.skidmore.edu>
2022-04-27 08:38:01 -0400

    changes to Username Proxy to account for case differences

--- src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/UsernameProxy.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/persistence/UsernameProxy.java
@@ -87,6 +87,8 @@ public class UsernameProxy {
 
                 String tempban = prop.getProperty(altProp);
                 tempban = tempban.trim();
+                tempban = tempban.toLowerCase();
+                username = tempban.toLowerCase();
 
                 if (username.contains(tempban)) {
                     usernameIsGood = false;
