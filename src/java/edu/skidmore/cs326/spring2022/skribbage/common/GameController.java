04d91642a6ba983a5afa4ce5779990d280c7f4d0
ACarney <alexander.s.carney.ug@dartmouth.edu>
2022-04-27 14:27:01 -0400

    Added states + transitions to GameController

--- src/java/edu/skidmore/cs326/spring2022/skribbage/common/GameController.java
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/common/GameController.java
@@ -1,10 +1,12 @@
 package edu.skidmore.cs326.spring2022.skribbage.common;
 
 import edu.skidmore.cs326.spring2022.skribbage.common.events.CribbageEvent;
+import edu.skidmore.cs326.spring2022.skribbage.frontend.AnimationManager;
 import org.apache.log4j.Logger;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.Arrays;
 
 /**
  * The finite state machine implementation for the Skribbage game loop.
@@ -13,27 +15,113 @@ import java.beans.PropertyChangeListener;
  *
  * @author Alex Carney
  */
-public class GameController implements PropertyChangeListener {
+public final class GameController implements PropertyChangeListener {
     /**
      * Log.
      */
     private static final Logger LOG;
 
+    /**
+     * private static final instance for eager singleton.
+     */
+    private static final GameController INSTANCE;
+
     static {
         LOG = Logger.getLogger(GameController.class);
+        INSTANCE = new GameController();
     }
 
-    private EventFactory eventFactory = EventFactory.getInstance();
+    /**
+     * State of the game.
+     */
+    private GameState currentGameState;
 
+    /**
+     * Event factory for firing events.
+     */
+    private final EventFactory eventFactory;
+
+    /**
+     * Animation manager for rendering the front end.
+     */
+    private final AnimationManager animationManager;
+
+    /**
+     * Private constructor for singleton.
+     */
+    private GameController() {
+        eventFactory = EventFactory.getInstance();
+        animationManager = AnimationManager.getInstance();
+        currentGameState = GameState.START_GAME;
+    }
+
+    /**
+     * Fired whenever an event that this listener is listening to is caught.
+     * Should always be a CribbageEvent game, otherwise a ClassCastException
+     * will be thrown
+     *
+     * @param evt The caught event.
+     * @throws ClassCastException Caught event is not of type CribbageEvent
+     */
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         LOG.trace("GameController caught event: " + evt);
 
         CribbageEvent cribbageEvent = ((CribbageEvent) evt);
 
-        switch(cribbageEvent.getEventType()) {
+        switch (cribbageEvent.getEventType()) {
+            case PLAYER_CLICK_START_GAME:
+                LOG.debug("caught a player click start game event " + evt);
+                if (currentGameState == GameState.START_GAME) {
+                    currentGameState = GameState.CUT_DECK;
+                    animationManager.dealCards();
+                }
+                break;
+            case PLAYER_CLICK_DECK:
+                LOG.debug("caught a player click start game event " + evt);
+                if (currentGameState == GameState.CUT_DECK) {
+                    currentGameState = GameState.DISCARD_TO_CRIB;
+                    //TODO: response
+                }
+                break;
+            case PLAYER_SEND_CARDS_TO_CRIB:
+                LOG.debug("caught a player send cards to crib event " + evt);
+                if (currentGameState == GameState.CUT_DECK) {
+                    currentGameState = GameState.STARTER_CARD;
+                    //TODO: response
+                }
+                break;
+            case PLAYER_SELECT_START_CARD:
+                LOG.debug("player played their starting card event " + evt);
+                if (currentGameState == GameState.DISCARD_TO_CRIB) {
+                    currentGameState = GameState.PLAY_CARD;
+                    //TODO: response
+                }
+                break;
+            case PLAYER_PLAY_CARD:
+                LOG.debug("player played card to game " + evt);
+                // A nicer way to compare multiple things for equality, rather
+                // than doing state == a or state == b or ...
+                if (Arrays.asList(GameState.STARTER_CARD, GameState.PLAY_CARD)
+                    .contains(currentGameState)) {
+                    currentGameState = GameState.PLAY_CARD;
+                    //TODO: response
+                }
 
+                break;
+            default:
+                LOG.error(
+                    "Caught an event that could not be handled. Reached default statement " + evt);
+                break;
         }
+    }
 
+    /**
+     * Gets the state of this game.
+     *
+     * @return the state of the game.
+     */
+    public GameState getCurrentGameState() {
+        return currentGameState;
     }
 }
---
+++ src/java/edu/skidmore/cs326/spring2022/skribbage/frontend/events/game/PlayerSelectStartCardEvent.java
@@ -0,0 +1,36 @@
+package edu.skidmore.cs326.spring2022.skribbage.frontend.events.game;
+
+import edu.skidmore.cs326.spring2022.skribbage.common.EventType;
+import edu.skidmore.cs326.spring2022.skribbage.common.Player;
+import edu.skidmore.cs326.spring2022.skribbage.common.events.CribbageEvent;
+
+/**
+ * Event fired when the user clicks 'start game' for the first time.
+ */
+public class PlayerSelectStartCardEvent extends CribbageEvent {
+
+    /**
+     * This event type.
+     */
+    private static final EventType EVENT_TYPE =
+        EventType.PLAYER_SELECT_START_CARD;
+
+    /**
+     * Event associated with a user attempting to start a game.
+     *
+     * @param source bean that fired the event.
+     * @param args   Player object
+     * @author Alex Carney
+     */
+    protected PlayerSelectStartCardEvent(Object source, Object... args) {
+        super(source, EVENT_TYPE, (Player) args[0]);
+    }
+
+    /**
+     * @return event type.
+     */
+    @Override
+    public EventType getEventType() {
+        return EVENT_TYPE;
+    }
+}
